60a8c5091a3eb5fada65fca97a71613a828d3f9f
jjensen-ai <jordon.jensen@live.ca>
2023-04-09 23:53:06 -0400

    showing all employees

--- src/app/Components/employees/employees.component.ts
+++ src/app/Components/employees/employees.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmployeeService } from 'src/app/services/employeeServices/employee.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.css'],
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit {
+  employees: any = [];
 
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.employeeService.getAllEmployees().subscribe((employees) => {
+      this.employees = employees.data.getAllEmployees;
+    });
+  }
 }
--- src/app/services/employeeServices/employee.service.ts
+++ src/app/services/employeeServices/employee.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { ApolloQueryResult } from '@apollo/client/core';
+import { Apollo, MutationResult, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+
+const GETEMPLOYEES = gql`
+  query GetAllEmployees {
+    getAllEmployees {
+      id
+      firstName
+      lastName
+      email
+      gender
+      salary
+    }
+  }
+`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private apollo: Apollo) {}
 
-  constructor() { }
+  getAllEmployees(): Observable<ApolloQueryResult<any>> {
+    return this.apollo.watchQuery<any>({
+      query: GETEMPLOYEES,
+    }).valueChanges;
+  }
 }
